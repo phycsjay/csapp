24a9520324c4a95787239c3fb724a72f30950e5f
Hu Minjie <huminjie_hust@163.com>
2017-05-29 16:28:52 +0800

    ilog2

--- datalab/datalab-handout/bits.c
+++ datalab/datalab-handout/bits.c
@@ -335,7 +335,7 @@ int ilog2(int x) {
  */
 unsigned float_neg(unsigned uf) {
   unsigned of;
-  if(uf != 0x7f800000 && ((uf & 0x7f800000) == 0x7f800000)) of = uf;
+  if(uf != 0x7f800000 && uf != 0xff800000 && ((uf & 0x7f800000) == 0x7f800000)) of = uf;
   else of = uf ^ 0x80000000;
   return of;
 }
